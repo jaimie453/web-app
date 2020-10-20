261a04a7274a1e38c9b6899bd2077bea51d5c12f
Jaimie Bowen <jbowen22_stu@kent.edu>
2020-10-19 23:26:17 -0400

    changed message

--- bottle_app.py
+++ bottle_app.py
@@ -1,10 +1,10 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route
+from bottle import default_app, route, run
 
 @route('/')
 def hello_world():
-    return 'Hello from Bottle!'
+    return 'Hello World!'
 
 application = default_app()
-
+run(host='localhost', port=8080, debug=True)
